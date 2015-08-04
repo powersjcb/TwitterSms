edf7e40b86c8ce2da97abb21e444ff26fd186207
Jacob Powers <powersjcb@gmail.com>
2015-08-03 22:48:33 -0700

    Switch from get to post requests on sms api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     post '/tweets', to: 'tweets#create'
-    get '/sms', to: 'sms#receive_sms'
+    post '/sms', to: 'sms#receive_sms'
   end
 
   get '/', to: 'static_pages#app'
