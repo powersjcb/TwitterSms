6d41824d1f2b20ce5681a208cfb4e2cfe58a7994
Jacob Powers <powersjcb@gmail.com>
2015-08-03 22:03:25 -0700

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     post '/tweets', to: 'tweets#create'
-    get 'sms', to: 'twilio#receive_sms'
+    get '/sms', to: 'twilio#receive_sms'
   end
 
   get '/', to: 'static_pages#app'
